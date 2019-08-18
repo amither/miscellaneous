fb821a348bef0125ec4a190c518b3b5f5be7e554
amither <leekdumpling@gmail.com>
2019-08-18 10:39:02 +0800

    最长公共子串, 改进

--- cpp/lcsub.cpp
+++ cpp/lcsub.cpp
@@ -52,8 +52,42 @@ void f(const string &s1, const string &s2)
     cout << s1.substr(maxIndex + 1 - maxLength, maxLength) << endl;
 }
 
+void f1(const string &s1, const string &s2)
+{
+    int maxLength = 0; 
+    int maxIndex = 0;
+    int row = 0;
+    int col = s1.length() -1;
+    while(row < s2.length())
+    {
+        int len = 0;
+        for (int i = row, j = col; i < s2.length() && j < s1.length(); i++, j++)
+        {
+            if (s2[i] == s1[j])
+            {
+                len++;
+                if (len > maxLength)
+                {
+                    maxLength = len;
+                    maxIndex = j;
+                }
+            }
+            else
+                len = 0;
+        }
+
+        if (col > 0)
+            col--;
+        else
+            row++;
+    }
+    cout << maxLength << endl;
+    cout << s1.substr(maxIndex + 1 - maxLength, maxLength) << endl;
+}
+
 int main(int argc, char *argv[])
 {
     f(argv[1], argv[2]);
+    f1(argv[1], argv[2]);
     return 0;
 }
